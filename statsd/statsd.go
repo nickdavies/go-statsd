eea85d4e916195e8ed04dd3c24cd3fa0df35bc8b
Nick Davies <git@nicolasdavies.com.au>
2013-11-23 13:53:56 +1100

    Wrote basic main function to get an idea of how well it all fits together

---
+++ statsd/statsd.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+    "time"
+    "fmt"
+)
+
+import (
+    "./servers"
+    "./metrics"
+)
+
+func main() {
+
+    server, err := servers.GetServer("udp")
+    if err != nil {
+        panic(err)
+    }
+
+    inbound, _, err := server.Run(servers.Config{":1234"})
+    if err != nil {
+        panic(err)
+    }
+
+    rawMetrics := make(chan *metrics.Metric)
+
+    collector, rawStats := metrics.NewMetricCollector(rawMetrics, 1)
+
+    flushInterval := 1 * time.Second
+
+    go func () {
+        for _ = range time.Tick(flushInterval) {
+            collector.Flush()
+        }
+    }()
+
+    stats := metrics.ProcessFromChannel(rawStats, flushInterval, 2)
+
+    go func () {
+        for _ = range stats {
+            // write to backend??
+        }
+    }()
+
+    for p := range inbound {
+        fmt.Println("Recv: ", p.Message)
+
+        ms, err := metrics.ParseMessage(p.Message)
+        if err != nil {
+            //TODO: Deal with error
+            continue
+        }
+        for _, m := range ms {
+            rawMetrics <-m
+        }
+
+        server.DestroyPacket(p)
+    }
+
+}
