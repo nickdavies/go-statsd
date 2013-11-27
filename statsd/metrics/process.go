0b51d2f24fa724046afd76349c9aabcd1b31211d
Nick Davies <git@nicolasdavies.com.au>
2013-11-28 08:30:34 +1100

    Added work from go meetup

--- statsd/metrics/process.go
+++ statsd/metrics/process.go
@@ -2,12 +2,37 @@ package metrics
 
 import (
     "time"
+    "sort"
+    "math"
 )
 
 type ProcessedStats struct {
     StatsCollection
 
     CounterRates map[string]float64
+
+    ExtraTimerData map[string]TimerData
+}
+
+type TimerData struct {
+    Std float64
+    Upper float64
+    Lower float64
+    Count float64
+    Count_ps float64
+    Sum float64
+    Mean float64
+    Median float64
+
+    Percents []TimerPercentile
+}
+
+type TimerPercentile struct {
+    Percent float64
+
+    Mean float64
+    Sum float64
+    Boundry float64
 }
 
 func NewProcessedStats() *ProcessedStats {
@@ -26,6 +51,7 @@ func ProcessFromChannel(inbound <-chan *StatsCollection, flushInterval time.Dura
             }
         }()
     }
+
     return out
 }
 
@@ -38,5 +64,59 @@ func ProcessStats(rawStats *StatsCollection, flushInterval time.Duration) *Proce
         stats.CounterRates[key] = value / flushSeconds
     }
 
+    for key, values := range rawStats.Timers {
+
+        data := TimerData{
+            Percents: make([]TimerPercentile)
+        }
+
+        sort.Float64s(values)
+
+        data.Sum = cumulativeValues[-1]
+        data.Min
+
+        var count = len(values)
+        var min = values[0]
+        var max = values[-1]
+
+        var cumulativeValues = make([]float64, 0, count)
+
+        cumulativeValues[0] = min
+        for i := 1; i < count; i++ {
+            cumulativeValues[i] = values[i] + cumulativeValues[i-1]
+        }
+
+        for _, percent := range percentThreshold {
+            pData := {
+                Percent:  percent,
+
+                Sum: min,
+                Mean: min,
+                Boundry: max,
+            }
+
+            if count > 1 {
+                var numInThreshold = math.Floor(math.Abs(percent) / 100 * count)
+                if numInThreshold == 0 {
+                    continue
+                }
+
+                if (percent > 0) {
+                    pData.Boundry = values[numInThreshold - 1]
+                    pData.Sum = cumulativeValues[numInThreshold - 1]
+                } else {
+                    pData.Boundry = values[count - numInThreshold]
+                    pData.Sum = cumulativeValues[count - 1] - cumulativeValues[count - numInThreshold - 1]
+                }
+                pData.Mean = pData.Sum / numInThreshold
+            }
+            data.Percents = append(data.Percents, pData)
+        }
+
+        // process_metrics.js +69
+
+    }
+
     return stats
 }
+
